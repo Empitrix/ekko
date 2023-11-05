f3cc84c4f654b8c055103ae6b4f3519e4ee26e7a
Empitrix <empitrix@gmail.com>
2023-11-05 19:58:03 +0330

    Added: Database Functions Interface

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,38 +1,23 @@
-import 'package:ekko/config/public.dart';
 import 'package:flutter/material.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart' as sq;
 import 'package:sqflite/sqflite.dart';
-import 'dart:io';
-
-
-/* Create, Read, Update, Delete */
-Future<Database> _createDB() async {
-	Database? db;
-	if(Platform.isWindows || Platform.isLinux){
-		sq.sqfliteFfiInit();
-		db = await sq.databaseFactoryFfi.openDatabase(dbPath);
-	} else {
-		db = await sq.openDatabase(dbPath);
-	}
-	return db;
-}
+import 'package:ekko/database/dfi.dart';
 
 
 class DB {
 	Future<void> init() async {
 		/* Initialized database */
-		Database db = await _createDB();
+		Database db = await createDB();
 		await db.execute("""
 			CREATE TABLE IF NOT EXISTS local (
-				darkMode bit
+				darkMode bit,
+				acrylicMode bit
 			)
 		""");
-		
 		if(List.from(await db.query("local")).isEmpty){
 			Map<String, Object?> initData = {
-				"darkMode": 0
+				"darkMode": 0,
+				"acrylicMode": 0
 			};  // Init table
-
 			// Set parameters on db 
 			await db.insert("local", initData);
 			debugPrint("[DATABASE INITIALIZED]");
@@ -44,19 +29,14 @@ class DB {
 
 
 	Future<ThemeMode> readTheme() async {
-		Database db = await _createDB();
-		List<Map> data = await db.query("local");
-		await db.close();
-		return data[0]["darkMode"] == 1 ?
+		Map data  = await getQuery("local");
+		return data["darkMode"] == 1 ?
 			ThemeMode.dark : ThemeMode.light;
 	}
 
 	Future<void> updateTheme(ThemeMode newMode) async {
-		Database db = await _createDB();
-		await db.update('local', {
-			"darkMode": newMode == ThemeMode.dark ? 1 : 0
-		});
-		await db.close();
+		await updateTable('local',
+			{"darkMode": newMode == ThemeMode.dark ? 1 : 0});
 	}
 
 
---
+++ lib/database/dfi.dart
@@ -0,0 +1,30 @@
+/* Database Functions Interface */
+import 'package:ekko/config/public.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart' as sq;
+import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+
+Future<Database> createDB() async {
+	Database? db;
+	if(Platform.isWindows || Platform.isLinux){
+		sq.sqfliteFfiInit();
+		db = await sq.databaseFactoryFfi.openDatabase(dbPath);
+	} else {
+		db = await sq.openDatabase(dbPath);
+	}
+	return db;
+}
+
+Future<Map> getQuery(String tName) async {
+	Map data = {};
+	Database db = await createDB();
+	data = (await db.query(tName)).first;
+	await db.close();
+	return data;
+}
+
+Future<void> updateTable(String tName, Map<String, Object?> data) async {
+	Database db = await createDB();
+	await db.update(tName, data);
+	await db.close();
+}
