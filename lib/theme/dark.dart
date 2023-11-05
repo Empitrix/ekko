12f65bc070ef97d34ab673569ad8d52e68c76d8a
Empitrix <empitrix@gmail.com>
2023-11-05 20:28:18 +0330

    Added: Fonts

--- lib/theme/dark.dart
+++ lib/theme/dark.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData dark(){
 	return ThemeData(
 		useMaterial3: true,
+    fontFamily: "Rubik",
 		colorScheme: ColorScheme.fromSeed(
 			seedColor: Colors.cyan,
 			brightness: Brightness.dark
--- lib/theme/light.dart
+++ lib/theme/light.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData ligth(){
 	return ThemeData(
 		useMaterial3: true,
+    fontFamily: "Rubik",
 		colorScheme: ColorScheme.fromSeed(
 			seedColor: Colors.blue,
 			brightness: Brightness.light
