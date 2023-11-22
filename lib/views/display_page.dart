96a1f60b83bfd494efa25d77454bfb88a59f922a
Empitrix <empitrix@gmail.com>
2023-11-23 03:15:58 +0330

    Added: Hide on scroll animation for AppBar on Display-Page

--- lib/views/display_page.dart
+++ lib/views/display_page.dart
@@ -43,7 +43,7 @@ class _DisplayPageState extends State<DisplayPage> with TickerProviderStateMixin
 		floatingButtonAnim = generateLinearAnimation(
 			ticket: this, initialValue: 1, durations: [1000]);
 		appbarHideAnimation = generateLinearAnimation(
-			ticket: this, initialValue: 1, durations: [1000]);
+			ticket: this, initialValue: 56, durations: [100], range: {0, 56});
 	}
 
 	void initListeners(){
@@ -77,6 +77,7 @@ class _DisplayPageState extends State<DisplayPage> with TickerProviderStateMixin
 	void initState() {
 		// Load async
 		initAnimations();
+		initListeners();
 		loadAll();
 		super.initState();
 	}
@@ -113,70 +114,75 @@ class _DisplayPageState extends State<DisplayPage> with TickerProviderStateMixin
 				// 	),
 				// ),
 
-				body: NestedScrollView(
-					floatHeaderSlivers: true,
-					headerSliverBuilder: (BuildContext context, bool innerBoxInScrolled){
-						return [
-							SliverAppBar(
-								automaticallyImplyLeading: false,
-								// floating: true, snap: true,
-								title: const Text("Dispaly"),
-								leading: IconButton(
-									icon: const Icon(Icons.close),
-									onPressed: (){
-										_backToPreviousPage();
-									},
-								),
-							)
-						];
-					},
-					body: Builder(
-						builder: (BuildContext context){
-
-							if(!isLoaded){
-								return const Center(
-									child: CircularProgressIndicator(),
-								);
-							}
-
-							return SelectionArea(
-								child: ListView(
-									controller: scrollCtrl,
-									padding: const EdgeInsets.only(
-										right: 12, left: 12,
-										top: 12, bottom: 85  // :)
-									),
-									children: [
-										/* Title */
-										Text(
-											note!.title,
-											style: const TextStyle(
-												fontSize: 25,
-												fontWeight: FontWeight.w500,
-												height: 0
-											),
-										),
-										const SizedBox(height: 5),
-										/* Description */
-										Text(
-											note!.description,
-											style: TextStyle(
-												fontSize: 20,
-												color: Theme.of(context)
-													.colorScheme.inverseSurface
-													.withOpacity(0.73),
-												fontWeight: FontWeight.w500,
-												height: 0
-											),
-										),
-										const SizedBox(height: 10),
-										MDGenerator(content: note!.content, textHeight: 1.4)
-									],
-								),
+				body: Builder(
+					builder: (BuildContext context){
+						if(!isLoaded){
+							return const Center(
+								child: CircularProgressIndicator(),
 							);
+						}
+						return Column(
+							mainAxisAlignment: MainAxisAlignment.start,
+							crossAxisAlignment: CrossAxisAlignment.start,
+							children: [
 
-						},
-					),
+								AnimatedBuilder(
+									animation: appbarHideAnimation!.animation,
+									builder: (BuildContext context, Widget? child) => AppBar(
+										automaticallyImplyLeading: false,
+										toolbarHeight: appbarHideAnimation!.animation.value,
+										title: const Text("Dispaly"),
+										leading: IconButton(
+											icon: const Icon(Icons.close),
+											onPressed: (){
+												_backToPreviousPage();
+											},
+										),
+									),
+								),
+
+								Expanded(
+									child: SelectionArea(
+										child: ListView(
+											controller: scrollCtrl,
+											padding: const EdgeInsets.only(
+												right: 12, left: 12,
+												top: 12, bottom: 85  // :)
+											),
+											children: [
+												/* Title */
+												Text(
+													note!.title,
+													style: const TextStyle(
+														fontSize: 25,
+														fontWeight: FontWeight.w500,
+														height: 0
+													),
+												),
+												const SizedBox(height: 5),
+												/* Description */
+												Text(
+													note!.description,
+													style: TextStyle(
+														fontSize: 20,
+														color: Theme.of(context)
+															.colorScheme.inverseSurface
+															.withOpacity(0.73),
+														fontWeight: FontWeight.w500,
+														height: 0
+													),
+												),
+												const SizedBox(height: 10),
+												MDGenerator(content: note!.content, textHeight: 1.4)
+											],
+										),
+									)
+								)
+
+							],
+						);
+
+					},
 				)
 			),
 		);
