aa5a5cb3ebf6b6e2652b50f6b4268ee70149ab23
Empitrix <empitrix@gmail.com>
2023-12-12 23:08:56 +0330

    Added: shortcut for search-bar (CTRL+F)

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -2,6 +2,7 @@ import 'package:ekko/animation/expand.dart';
 import 'package:ekko/backend/backend.dart';
 import 'package:ekko/backend/extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
 AppBar customSearchBar({
@@ -16,9 +17,9 @@ AppBar customSearchBar({
 	required Function onLoading,
 	required Widget leading}){
 
-	// FocusNode focus = FocusNode();
+	FocusNode shortcutFocus = FocusNode();
 	ValueNotifier<double> openNotifier =
-	ValueNotifier<double>(searchAnim.animation.value);
+		ValueNotifier<double>(searchAnim.animation.value);
 	ValueNotifier<bool> textNotifier = ValueNotifier(true);
 	searchAnim.animation.addListener((){
 		openNotifier.value = searchAnim.animation.value;
@@ -27,6 +28,26 @@ AppBar customSearchBar({
 	controller.addListener((){
 		textNotifier.value = controller.text == "";
 	});
+
+
+	Future<void> __toggle() async {
+		if(!textNotifier.value){
+			controller.text = "";
+			onChange(controller.text);
+			focus.requestFocus();
+			return;
+		}
+		if(searchAnim.animation.value == 1){
+			await searchAnim.controller.reverse();
+			onClose();
+			shortcutFocus.requestFocus();
+			// focus.unfocus();
+		}else{
+			await searchAnim.controller.forward();
+			onOpen();
+			// focus.requestFocus();
+		}
+	}
 
 	return AppBar(
 		leading: IconButton(
@@ -53,13 +74,34 @@ AppBar customSearchBar({
 							borderRadius: BorderRadius.circular(5),
 							color: Theme.of(context).scaffoldBackgroundColor.aae(context)
 						),
-						child: TextField(
-							focusNode: focus,
-							controller: controller,
-							onChanged: (value) => onChange(value),
-							decoration: const InputDecoration(
-								border: InputBorder.none,
-								hintText: "Search..."
+						// child: TextField(
+						// 	focusNode: focus,
+						// 	controller: controller,
+						// 	onChanged: (value) => onChange(value),
+						// 	decoration: const InputDecoration(
+						// 		border: InputBorder.none,
+						// 		hintText: "Search..."
+						// 	),
+						// ),
+						child: CallbackShortcuts(
+								bindings: <ShortcutActivator, VoidCallback>{
+									const SingleActivator(LogicalKeyboardKey.keyF, control: true): () async {
+										await __toggle();
+										// debugPrint("Awesome");
+								},
+							},
+							child: Focus(
+								autofocus: true,
+								focusNode: shortcutFocus,
+								child: TextField(
+									focusNode: focus,
+									controller: controller,
+									onChanged: (value) => onChange(value),
+									decoration: const InputDecoration(
+										border: InputBorder.none,
+										hintText: "Search..."
+									),
+								),
 							),
 						),
 					)
@@ -77,6 +119,54 @@ AppBar customSearchBar({
 			],
 		),
 		actions: [
+			// CallbackShortcuts(
+			// 	bindings: <ShortcutActivator, VoidCallback>{
+			// 		const SingleActivator(LogicalKeyboardKey.keyF, control: true): () {
+			// 		},
+			// 	},
+			// 	child: Focus(
+			// 		autofocus: true,
+			// 		child: Container(
+			// 			margin: const EdgeInsets.all(8),
+			// 			child: ValueListenableBuilder(
+			// 				valueListenable: openNotifier,
+			// 				builder: (_, isOpenV, __){
+			// 					bool isOpen = isOpenV == 1;
+			// 					return ValueListenableBuilder(
+			// 						valueListenable: textNotifier,
+			// 						builder: (_, isEmpty, __){
+			// 							return IconButton(
+			// 								icon: Icon(
+			// 									isOpen ?
+			// 									isEmpty ? Icons.search :
+			// 									Icons.close :
+			// 									Icons.search
+			// 								),
+			// 								onPressed: () async {
+			// 									await __toggle();
+			// 									// if(!isEmpty){
+			// 									// 	controller.text = "";
+			// 									// 	onChange(controller.text);
+			// 									// 	return;
+			// 									// }
+			// 									// if(searchAnim.animation.value == 1){
+			// 									// 	await searchAnim.controller.reverse();
+			// 									// 	onClose();
+			// 									// }else{
+			// 									// 	await searchAnim.controller.forward();
+			// 									// 	onOpen();
+			// 									// }
+			// 								}
+			// 							);
+			// 						}
+			// 					);
+			// 				},
+			// 			),
+			// 		)
+			// 	)
+			// ),
+
+
 			Container(
 				margin: const EdgeInsets.all(8),
 				child: ValueListenableBuilder(
@@ -94,6 +184,7 @@ AppBar customSearchBar({
 										Icons.search
 									),
 									onPressed: () async {
+										// await __toggle();
 										if(!isEmpty){
 											controller.text = "";
 											onChange(controller.text);
@@ -101,13 +192,11 @@ AppBar customSearchBar({
 										}
 										if(searchAnim.animation.value == 1){
 											await searchAnim.controller.reverse();
-											// focus.unfocus();
 											onClose();
-											// FocusManager.instance.primaryFocus!.unfocus();
+											shortcutFocus.requestFocus();
 										}else{
 											await searchAnim.controller.forward();
 											onOpen();
-											// focus.requestFocus();
 										}
 									}
 								);
@@ -116,6 +205,7 @@ AppBar customSearchBar({
 					},
 				),
 			)
+
 		],
 	);
 }
