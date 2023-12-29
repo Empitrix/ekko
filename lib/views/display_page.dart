cb946631c86eb21349679b84459df8b4fe7dabd2
Empitrix <empitrix@gmail.com>
2023-12-29 19:36:14 +0330

    Edited: Appbar view in Display page and Removed Headline Texts from display page

--- lib/views/display_page.dart
+++ lib/views/display_page.dart
@@ -102,39 +102,84 @@ class _DisplayPageState extends State<DisplayPage> with TickerProviderStateMixin
 									previousPage: widget,
 								)),
 						),
-						body: NestedScrollView(
-							controller: scrollCtrl,
-							floatHeaderSlivers: true,
-							physics: const ScrollPhysics(),
-							headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
-								return <Widget>[
-									SliverAppBar(
-										floating: false,
-										pinned: false,
-										title: const Text("Display"),
-										forceElevated: false,
-										leading: IconButton(
-											icon: const Icon(Icons.close),
-											onPressed: (){
-												_backToPreviousPage();
-											},
-										),
-									)
-								];
-							},
-							body: Builder(
-								builder: (context){
-									if(!isLoaded){
-										return const Center(
-											child: CircularProgressIndicator(),
-										);
-									}
-									return SelectionArea(
-										// onSelectionChanged: (SelectedContent? txt){
-										// 	if(txt != null){
-										// 		debugPrint(txt.plainText);
-										// 	}
-										// },
+						body: Builder(
+							builder:(context){
+
+								if(!isLoaded){
+									return const Center(child: CircularProgressIndicator());
+								}
+
+								return NestedScrollView(
+									controller: scrollCtrl,
+									floatHeaderSlivers: true,
+									physics: const ScrollPhysics(),
+									headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
+										return <Widget>[
+											SliverAppBar(
+												floating: false,
+												pinned: false,
+												// title: const Text("Display"),
+												title: Column(
+													mainAxisAlignment: MainAxisAlignment.start,
+													crossAxisAlignment: CrossAxisAlignment.start,
+													children: [
+														Text(
+															note!.title,
+															overflow: TextOverflow.fade,
+															style: Theme.of(context).appBarTheme.titleTextStyle
+														),
+														Text(note!.description, style: TextStyle(
+															fontSize: 16,
+															overflow: TextOverflow.fade,
+															color: dMode ? Colors.grey : Colors.grey[400]
+														))
+													],
+												),
+												// flexibleSpace: FlexibleSpaceBar(
+												// 	stretchModes: const <StretchMode>[
+												// 		StretchMode.zoomBackground,
+												// 		StretchMode.blurBackground,
+												// 	],
+												// 	background: Column(
+												// 		mainAxisAlignment: MainAxisAlignment.start,
+												// 		crossAxisAlignment: CrossAxisAlignment.start,
+												// 		children: [
+												// 			Text(
+												// 				note!.title,
+												// 				overflow: TextOverflow.fade,
+												// 				style: Theme.of(context).appBarTheme.titleTextStyle
+												// 			),
+												// 			Text(note!.description, style: TextStyle(
+												// 				fontSize: 16,
+												// 				overflow: TextOverflow.fade,
+												// 				color: dMode ? Colors.grey : Colors.grey[400]
+												// 			))
+												// 		],
+												// 	),
+												// ),
+												// 
+												// bottom: AppBar(title: const Text("Awesome")),
+												// expandedHeight: 200,
+												actions: [
+													Container(
+														margin: const EdgeInsets.all(5),
+														child: IconButton(
+															icon: const Icon(Icons.more_vert),
+															onPressed: (){}
+														),
+													)
+												],
+												forceElevated: false,
+												leading: IconButton(
+													icon: const Icon(Icons.close),
+													onPressed: (){
+														_backToPreviousPage();
+													},
+												),
+											)
+										];
+									},
+									body: SelectionArea(
 										focusNode: contextFocus,
 										contextMenuBuilder: (context, editableTextState){
 											final List<ContextMenuButtonItem> buttonItems = editableTextState.contextMenuButtonItems;
@@ -149,35 +194,35 @@ class _DisplayPageState extends State<DisplayPage> with TickerProviderStateMixin
 												top: 12, bottom: 85  // :)
 											),
 											children: [
-												/* Title */
-												Text(
-													note!.title,
-													style: const TextStyle(
-														fontSize: 25,
-														fontWeight: FontWeight.w500,
-														height: 0
-													),
-												),
-												const SizedBox(height: 5),
-												/* Description */
-												Text(
-													note!.description,
-													style: TextStyle(
-														fontSize: 20,
-														color: Theme.of(context)
-															.colorScheme.inverseSurface
-															.withOpacity(0.73),
-														fontWeight: FontWeight.w500,
-														height: 0
-													),
-												),
+												// /* Title */
+												// Text(
+												// 	note!.title,
+												// 	style: const TextStyle(
+												// 		fontSize: 25,
+												// 		fontWeight: FontWeight.w500,
+												// 		height: 0
+												// 	),
+												// ),
+												// const SizedBox(height: 5),
+												// /* Description */
+												// Text(
+												// 	note!.description,
+												// 	style: TextStyle(
+												// 		fontSize: 20,
+												// 		color: Theme.of(context)
+												// 			.colorScheme.inverseSurface
+												// 			.withOpacity(0.73),
+												// 		fontWeight: FontWeight.w500,
+												// 		height: 0
+												// 	),
+												// ),
 												const SizedBox(height: 10),
 												MDGenerator(content: note!.content)
 											],
 										),
-									);
-								},
-							)
+									)
+								);
+							}
 						),
 					)
 				)
