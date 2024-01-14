94aa4c2d669a360774ccf9e5a4e194698de341ce
Empitrix <empitrix@gmail.com>
2024-01-14 04:37:15 +0100

    Fixed: 'DividerLine' rouness by adding enum for rounding

--- lib/components/divider.dart
+++ lib/components/divider.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+enum LineSide { right, left }
+
 class DividerLine extends StatelessWidget {
 	final double height;
 	final double? width;
 	final Color? color;
+	final LineSide lineSide;
 	const DividerLine({
 		super.key,
+		required this.lineSide,
 		this.height = 2,
 		this.width,
 		this.color});
@@ -16,7 +20,11 @@ class DividerLine extends StatelessWidget {
 			height: height,
 			decoration: BoxDecoration(
 				color: color ?? Theme.of(context).colorScheme.inverseSurface.withOpacity(0.5),
-				borderRadius: const BorderRadius.only(topRight: Radius.circular(5), bottomRight: Radius.circular(5))
+				borderRadius: lineSide == LineSide.right ?
+					const BorderRadius.only(
+						topLeft: Radius.circular(5), bottomLeft: Radius.circular(5)):
+					const BorderRadius.only(
+						topRight: Radius.circular(5), bottomRight: Radius.circular(5))
 			),
 		);
 	}
@@ -41,7 +49,7 @@ class TitleDivider extends StatelessWidget {
 				const SizedBox(height: 12),
 				Row(
 					children: [
-						DividerLine(height: height, width: 20),
+						DividerLine(height: height, width: 20, lineSide: LineSide.left),
 						const SizedBox(width: 12),
 						Text(
 							title,
@@ -51,7 +59,7 @@ class TitleDivider extends StatelessWidget {
 							),
 						),
 						const SizedBox(width: 12),
-						Expanded(child: DividerLine(height: height, width: 20))
+						Expanded(child: DividerLine(height: height, width: 20, lineSide: LineSide.right))
 					],
 				),
 				const SizedBox(height: 8),
