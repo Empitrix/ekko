2738f793dfe691c13740d2c5f11bf32553556c18
Empitrix <empitrix@gmail.com>
2024-01-31 19:39:17 +0100

    Added: checkbox highlight style for text-field

--- lib/markdown/filed_rules.dart
+++ lib/markdown/filed_rules.dart
@@ -98,48 +98,29 @@ List<RegexFormattingStyle> allFieldRules(BuildContext context){
 						TextSpan(text: lines.sublist(2).join("\n")), borderStyle, true)
 				]);
 			}
-		)
+		),
 
-
-		// Check-Box
-		// RegexPatternTextStyle(
-		// 	// regexPattern: r"^\s*\-\s{1}\[(\s{1}|\x)\]\s{1}",
-		// 	regexPattern: r"^\s*(-|\*|\+)\s{1}\[(\s{1}|\x)\]\s{1}",
-		// 	action: (txt, match){
-		// 		int openingIdx = txt.split("").indexOf("[") + 1; 
-		// 		return TextSpan(
-		// 			children: [
-		// 				TextSpan(
-		// 					text: txt.substring(0, openingIdx),
-		// 					style: const TextStyle(color: Colors.orange)),
-		// 				TextSpan(text: txt.substring(openingIdx, openingIdx + 1)),
-		// 				TextSpan(
-		// 					text: txt.substring(openingIdx + 1),
-		// 					style: const TextStyle(color: Colors.orange)),
-		// 			],
-		// 		);
-		// 	},
-		// ),
-		// 
-		// // Headline 1..6
-		// RegexPatternTextStyle(
-		// 	regexPattern: r"^#{1,6} [\s\S]*?$",
-		// 	action: (txt, match){
-		// 		int end = "#$txt".lastIndexOf("## ") + 1;
-		// 		return TextSpan(
-		// 			children: [
-		// 				TextSpan(
-		// 					text: txt.substring(0, end),
-		// 					style: const TextStyle(color: Colors.indigo, fontWeight: FontWeight.w900)
-		// 				),
-		// 				TextSpan(
-		// 					text: txt.substring(end),
-		// 					style: const TextStyle(fontWeight: FontWeight.w900)
-		// 				),
-		// 			],
-		// 		);
-		// 	},
-		// ),
+		// {@CheckBox}
+		RegexActionStyle(
+			regex: RegExp(r"^\s*(-|\*|\+)\s{1}\[(\s{1}|\x)\]\s{1}"),
+			style: const TextStyle(),
+			action: (txt, match){
+				int openingIdx = txt.split("").indexOf("[") + 1; 
+				return TextSpan(
+					children: [
+						TextSpan(
+							text: txt.substring(0, openingIdx),
+							style: const TextStyle(color: Colors.orange)),
+						TextSpan(text: txt.substring(openingIdx, openingIdx + 1),
+							style: const TextStyle(
+								color: Colors.cyan, fontWeight: FontWeight.bold)),
+						TextSpan(
+							text: txt.substring(openingIdx + 1),
+							style: const TextStyle(color: Colors.orange)),
+					],
+				);
+			},
+		),
 
 	];
 }
