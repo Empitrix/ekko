fd8613c4a2f781d63be4a760575c134597f0ce5a
Empitrix <empitrix@gmail.com>
2024-04-18 00:33:10 +0200

    Added: latex/test

---
+++ latex/test/clinet/main.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+import 'dart:typed_data';
+import 'dart:convert';
+import 'dart:io';
+
+
+void main() async {
+	File file = File(path.join(Directory.current.absolute.path, "tmp", 'file.png'));
+	if(!file.existsSync()){ file.createSync(recursive: true); }
+
+	http.Response res = await http.post(
+		Uri.parse("http://127.0.0.1:5000/latex"),
+		headers: { "Content-Type": "application/json" },
+		encoding: Encoding.getByName('UTF-8'),
+		body: JsonEncoder().convert({ "latex": r"$$f(x) = x^2 - x^\frac{1}{\pi}$$" })
+	);
+
+	if(res.statusCode == 200){
+		Map output = JsonDecoder().convert(res.body);
+		if(output['msg'] == null){
+			Uint8List data = Uint8List.fromList(List<int>.from(output['data']));
+			file.writeAsBytesSync(data);
+			print("File Written as: ${file.path}");
+		} else {
+			print("Failed: ${output['msg']}");
+		}
+	} else {
+		print("SERVER FAILED!(code ${res.statusCode})");
+	}
+}
+
---
+++ latex/test/server/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from sympy import preview
+from io import BytesIO
+
+
+app = Flask(__name__)
+
+@app.route("/latex", methods=['POST'])
+def index():
+	data = request.json
+	if data == None:
+		return jsonify({"msg": "json/application Error"})
+	if data['latex'] != None:
+		obj = BytesIO()
+		try:
+			preview(
+				data['latex'],
+				viewer='BytesIO',
+				filename='test.png',
+				euler=False,
+				output='png',
+				dvioptions=['-D','1200'],
+				outputbuffer=obj)
+			return jsonify({"data": [t for t in obj.getvalue()]})
+		except Exception as e:
+			return jsonify({"msg": f"Failed to Process\nMSG:{e}"})
+	else:
+		return jsonify({"msg": "'latex' not found"})
+	# return jsonify(data)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
