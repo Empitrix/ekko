6dc0857ff4863ee474a396e664fdf7face2cf764
Empitrix <empitrix@gmail.com>
2024-04-19 02:00:20 +0200

    Added: Inline/Display detection for LaTex

--- lib/markdown/inline/latex.dart
+++ lib/markdown/inline/latex.dart
@@ -1,21 +1,34 @@
+import 'package:ekko/config/manager.dart';
 import 'package:ekko/markdown/inline_module.dart';
 import 'package:ekko/markdown/latex_server.dart';
+import 'package:ekko/utils/calc.dart';
 import 'package:flutter/material.dart';
 import 'dart:typed_data';
+
+import 'package:provider/provider.dart';
 
 class InlineLatex extends InlineModule {
 	InlineLatex(super.opt, super.gOpt);
 
 	@override
 	InlineSpan span(String txt){
+
+		double defaultHeight = calcTextSize(gOpt.context, " ", Provider.of<ProviderManager>(gOpt.context).defaultStyle).height;
+		bool isInline = txt.substring(0, 2).replaceAll("\$", "").isNotEmpty;
+		// debugPrint("The $txt is ${isInline ? 'inline' : 'display'}");
 		return WidgetSpan(
 			child: FutureBuilder<Uint8List?>(
 				future: latexPngAPI(txt),
 				builder: (BuildContext context, AsyncSnapshot<Uint8List?> snap){
 					if(snap.hasData){
 						if(snap.data != null){
-							return Center(child: Image.memory(snap.data!));
-							// return Image.memory(snap.data!);
+							if(isInline){
+								return SizedBox(
+									height: defaultHeight - (28 * defaultHeight / 100),
+									child: Image.memory(snap.data!, filterQuality: FilterQuality.none)
+								);
+							}
+							return Center(child: Image.memory(snap.data!, filterQuality: FilterQuality.none));
 						}
 						return const SizedBox();
 					}
